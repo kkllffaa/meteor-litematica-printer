06e5111af939e086ee64a84d6cd9b0b59b38f818
kkllffaa <franciszek.zubel@wp.pl>
2021-09-16 00:14:11 +0200

    better item switching and proper placing blocks with direction data eg. log

--- src/main/java/com/kkllffaa/meteor_litematica_printer/Printer.java
+++ src/main/java/com/kkllffaa/meteor_litematica_printer/Printer.java
@@ -1,6 +1,5 @@
 package com.kkllffaa.meteor_litematica_printer;
 
-import com.kkllffaa.meteor_litematica_printer.Addon;
 import fi.dy.masa.litematica.world.SchematicWorldHandler;
 import fi.dy.masa.litematica.world.WorldSchematic;
 import meteordevelopment.meteorclient.events.world.TickEvent;
@@ -15,12 +14,18 @@ import meteordevelopment.meteorclient.utils.world.BlockIterator;
 import meteordevelopment.meteorclient.utils.world.BlockUtils;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.BlockState;
-import net.minecraft.client.world.ClientWorld;
+import net.minecraft.item.Item;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.function.Supplier;
 
 public class Printer extends Module {
+	
 	
 	private final SettingGroup sgGeneral = settings.getDefaultGroup();
 	
@@ -30,10 +35,8 @@ public class Printer extends Module {
 			.name("printing-range")
 			.description("Printing block place range.")
 			.defaultValue(2)
-			.min(1)
-			.max(6)
-			.sliderMin(0)
-			.sliderMax(6)
+			.min(1).sliderMin(0)
+			.max(6).sliderMax(6)
 			.build()
 	);
 	
@@ -41,16 +44,23 @@ public class Printer extends Module {
 			.name("printing-delay")
 			.description("Delay between printing blocks in ticks.")
 			.defaultValue(2)
-			.min(0)
-			.max(100)
-			.sliderMin(0)
-			.sliderMax(40)
+			.min(0).sliderMin(0)
+			.max(100).sliderMax(40)
 			.build()
 	);
 	
-	private final Setting<Boolean> swing_hand = sgGeneral.add(new BoolSetting.Builder()
-			.name("swing-hand")
-			.description("swing hand when placing blocks.")
+	private final Setting<Integer> bpt = sgGeneral.add(new IntSetting.Builder()
+			.name("blocks/tick")
+			.description("how many blocks place in 1 tick.")
+			.defaultValue(10)
+			.min(1).sliderMin(1)
+			.max(100).sliderMax(100)
+			.build()
+	);
+	
+	private final Setting<Boolean> advenced = sgGeneral.add(new BoolSetting.Builder()
+			.name("advenced")
+			.description("respect block rotation (exterimental).")
 			.defaultValue(false)
 			.build()
 	);
@@ -62,11 +72,10 @@ public class Printer extends Module {
 		super(Addon.CATEGORY, "litematica-printer", "description");
 	}
 	
-	private int timer = 0;
-	
+	private int timer, placed = 0;
 	private int usedslot = -1;
 	
-	@EventHandler
+	@EventHandler @SuppressWarnings("unused")
 	private void onTick(TickEvent.Post event) {
 		if (mc.player == null) return;
 		WorldSchematic worldSchematic = SchematicWorldHandler.getSchematicWorld();
@@ -76,12 +85,19 @@ public class Printer extends Module {
 		}
 		if (timer >= printing_delay.get()) {
 			BlockIterator.register(printing_range.get(), printing_range.get(), (pos, blockState) -> {
-				if (!mc.player.getBlockPos().isWithinDistance(pos, printing_range.get())) return;
+				if (!mc.player.getBlockPos().isWithinDistance(pos, printing_range.get()) || !blockState.isAir()) return;
 				BlockState required = worldSchematic.getBlockState(pos);
 				
 				if (!required.isAir() && blockState.getBlock() != required.getBlock()) {
-					if (place(required, pos)) {
-						if (printing_delay.get() <= 0) BlockIterator.disableCurrent();
+					if (advenced.get() ?
+							swichitem(required.getBlock().asItem(), () -> betterPlace(required, pos)) :
+							swichitem(required.getBlock().asItem(), () -> oldPlace(pos))
+					) {
+						placed++;
+						if (placed >= bpt.get()) {
+							BlockIterator.disableCurrent();
+							placed = 0;
+						}
 						timer = 0;
 					}
 				}
@@ -89,33 +105,99 @@ public class Printer extends Module {
 		}else timer++;
 	}
 	
-	private boolean place(BlockState required, BlockPos pos) {
+	public boolean betterPlace (BlockState required, BlockPos pos) {
+		if (mc.player == null || mc.interactionManager == null || mc.world == null) return false;
+		if (!BlockUtils.canPlace(pos)) return false;
+		
+		if (mc.world.isAir(pos) || mc.world.getBlockState(pos).getMaterial().isLiquid()) {
+			Direction direction = dir(required);
+			
+			if (direction == Direction.UP) {
+				return BlockUtils.place(pos, Hand.MAIN_HAND, mc.player.getInventory().selectedSlot, false, 0, false, true, false);
+			}
+			
+			BlockHitResult result = new BlockHitResult(
+					//fromblockpos(pos/*.offset(Direction.UP)*/),
+					new Vec3d(pos.getX()+0.5, pos.getY()+0.5, pos.getZ()+0.5).add(direction.getOffsetX()*0.5, direction.getOffsetY()*0.5, direction.getOffsetZ()*0.5),
+					direction,
+					pos.offset(direction.getOpposite()),
+					false
+			);
+
+			//BlockUtilsAccessor.place(result, Hand.MAIN_HAND, false);
+			
+			mc.interactionManager.interactBlock(mc.player, mc.world, Hand.MAIN_HAND, result);
+			
+			return true;
+		}else return false;
+	}
+	
+	private Direction dir(BlockState state) {
+		if (state.contains(Properties.FACING)) return state.get(Properties.FACING);
+		else if (state.contains(Properties.AXIS)) return Direction.from(state.get(Properties.AXIS), Direction.AxisDirection.POSITIVE);
+		else if (state.contains(Properties.HORIZONTAL_AXIS)) return Direction.from(state.get(Properties.HORIZONTAL_AXIS), Direction.AxisDirection.POSITIVE);
+		else return Direction.UP;
+	}
+	
+	public boolean oldPlace(BlockPos pos) {
 		if (mc.player == null) return false;
-		FindItemResult result = InvUtils.find(required.getBlock().asItem());
-		if (mc.player.getMainHandStack().getItem() == required.getBlock().asItem()) {
-			if (BlockUtils.place(pos, Hand.MAIN_HAND, mc.player.getInventory().selectedSlot, false, 0, swing_hand.get(), true, false)) {
+		return BlockUtils.place(pos, Hand.MAIN_HAND, mc.player.getInventory().selectedSlot, false, 0, false, true, false);
+	}
+	
+	private boolean swichitem(Item item, Supplier<Boolean> action) {
+		if (mc.player == null) return false;
+		int a = mc.player.getInventory().selectedSlot;
+		FindItemResult result = InvUtils.find(item);
+		if (mc.player.getMainHandStack().getItem() == item) {
+			if (action.get()) {
 				usedslot = mc.player.getInventory().selectedSlot;
 				return true;
 			}else return false;
+		} else if (usedslot != -1 && mc.player.getInventory().getStack(usedslot).getItem() == item) {
+			InvUtils.swap(usedslot);
+			if (action.get()) {
+				InvUtils.swap(a);
+				return true;
+			}else {
+				InvUtils.swap(a);
+				return false;
+			}
 		}else if (result.found()) {
 			if (result.isHotbar()) {
-				if (BlockUtils.place(pos, result, false, 0, swing_hand.get(), true, true)) {
-					usedslot = result.getSlot();
+				InvUtils.swap(result.getSlot());
+				if (action.get()) {
+					usedslot = mc.player.getInventory().selectedSlot;
+					InvUtils.swap(a);
 					return true;
-				}else return false;
-			}else {
-				if (usedslot != -1) {
-					InvUtils.move().from(result.getSlot()).toHotbar(usedslot);
-					return BlockUtils.place(pos, Hand.MAIN_HAND, usedslot, false, 0, swing_hand.get(), true, true);
 				}else {
-					FindItemResult empty = InvUtils.findEmpty();
-					if (empty.found() && empty.isHotbar()) {
-						InvUtils.move().from(result.getSlot()).toHotbar(empty.getSlot());
-						usedslot = empty.getSlot();
-						return BlockUtils.place(pos, Hand.MAIN_HAND, empty.getSlot(), false, 0, swing_hand.get(), true, true);
-					}else return false;
+					InvUtils.swap(a);
+					return false;
 				}
-			}
-		} else return false;
+			}else if (result.isMain()){
+				FindItemResult empty = InvUtils.findEmpty();
+				if (empty.found() && empty.isHotbar()) {
+					InvUtils.move().from(result.getSlot()).toHotbar(empty.getSlot());
+					InvUtils.swap(empty.getSlot());
+					if (action.get()) {
+						usedslot = mc.player.getInventory().selectedSlot;
+						InvUtils.swap(a);
+						return true;
+					}else {
+						InvUtils.swap(a);
+						return false;
+					}
+				} else if (usedslot != -1) {
+					InvUtils.move().from(result.getSlot()).toHotbar(usedslot);
+					InvUtils.swap(usedslot);
+					if (action.get()) {
+						InvUtils.swap(a);
+						return true;
+					}else {
+						InvUtils.swap(a);
+						return false;
+					}
+				}else return false;
+			}else return false;
+		}else return false;
 	}
 }
