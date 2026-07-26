03bb7fc09b5fb6615f9f3af661648328388de34c
Aneent0x29A <aneent0x29a@gmail.com>
2026-07-26 18:53:51 +0700

    chore: update to Minecraft 26.1.2
    - Update minecraft_version to 26.1.2
    - Update loader_version to 0.19.3
    - Update loom_version to 1.16-SNAPSHOT
    - Update litematica to 0.27.8, malilib to 0.28.8
    - Switch plugin from fabric-loom to net.fabricmc.fabric-loom
    - Remove mappings (MC 26.x is unobfuscated)
    - Replace modImplementation with implementation
    - Update Java target from 21 to 25
    - Fix Category constructor for new Meteor API (Supplier<ItemStack>)
    - Bump mod version to 1.5.0

--- src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
+++ src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Addon extends MeteorAddon {
 	public static final Logger LOG = LogManager.getLogger();
-	public static final Category CATEGORY = new Category("Printer", new ItemStack(Items.PINK_CARPET));
+	public static final Category CATEGORY = new Category("Printer", () -> new ItemStack(Items.PINK_CARPET));
 
 	@Override
 	public void onInitialize() {
